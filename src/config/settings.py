87b10f390574f121098beca0fc8e75af5ecc87fc
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-28 03:11:26 +0330

    config: use pydantic BaseSettings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,14 +1,17 @@
 import os
-# from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 
-class _Config():
+class _Config(BaseSettings):
     MONGODB_URL : str = os.environ.get("MONGODB_URL")
 
-    ACCOUNTS_SERVICE_API_KEY = os.environ.get("ACCOUNTS_SERVICE_API_KEY")
-    ACCOUNTS_SERVICE_BASE_URL = os.environ.get("ACCOUNTS_SERVICE_BASE_URL")
+    ACCOUNTS_SERVICE_API_KEY :str = os.environ.get("ACCOUNTS_SERVICE_API_KEY")
+    ACCOUNTS_SERVICE_BASE_URL : str= os.environ.get("ACCOUNTS_SERVICE_BASE_URL")
 
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
 
 
 SETTINGS = _Config()
