6df1630f1b25bb475e2a5bd4df7be31d3b33f951
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-28 02:26:24 +0330

    build: uvicorn internal run

--- src/main.py
+++ src/main.py
@@ -14,3 +14,8 @@ app.include_router(router)
 @app.get("/")
 async def index():
     return {}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8001, reload=True)
