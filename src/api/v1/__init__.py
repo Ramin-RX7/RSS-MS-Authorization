ff23d81913f9b4a1af0204717eaeb29d9ca1ae68
Ramin RX7 <rawmin.rx@gmail.com>
2023-11-01 11:32:41 +0330

    chore: update api.v1 routes returns

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,9 +1,9 @@
-from typing import Any
 from fastapi import APIRouter,Header,Depends
+from fastapi.responses import JSONResponse
 
 from schemas import Signup,Login,RefreshToken,JWTPayload
 from auth.jwt_auth.jwt_auth import JWTHandler
-from schemas.base import Result,Response
+from schemas.base import Result
 from services import AccountsService
 
 
@@ -15,8 +15,9 @@ account_service = AccountsService()
 
 
 
+
 @router.post("/signup/")
-async def signup(user_data:Signup) -> dict[str, Any]:
+async def signup(user_data:Signup):
     """signup route which validate user data and send it to "accounts" service
 
     Args:
@@ -29,8 +30,8 @@ async def signup(user_data:Signup) -> dict[str, Any]:
     """
     resp = await account_service.signup(user_data)
     if resp:
-        ...
-    return resp.model_dump()
+        return JSONResponse(resp.data, 201)
+    return JSONResponse(resp.error.model_dump(), 400 if resp.status is False  else 500)
 
 
 @router.post('/login/')
@@ -44,22 +45,21 @@ async def login(user_data: Login, user_agent:str=Header()):
 
     Returns:
     --------
-    `_type_`: _description_
+    `JsonResponse` (200): _If given credentials are correct, access_token and refresh_token will be returned_
     """
     res = await account_service.login(user_data)
     if res:
         user_email = res["data"]["email"]
         access,refresh = await jwt_object.login(user_email, user_agent)
-        return {   #? make this Result?
+        return {
             "access_token": access,
             "refresh_token": refresh
         }
-    # error occured
-    return res
+    return JSONResponse(res.error.model_dump(), 400 if res.status is False  else 500)
 
 
 @router.post('/refresh/')
-async def refresh(token:RefreshToken, user_agent:str=Header(None)) -> dict[str, str]:
+async def refresh(token:RefreshToken, user_agent:str=Header(None)):
     """Re-login route which is used when receiving 401 status code which means access\
     token is expired and refresh token has to be used to retrieve new tokens
 
@@ -70,13 +70,13 @@ async def refresh(token:RefreshToken, user_agent:str=Header(None)) -> dict[str, 
 
     Returns:
     --------
-    `dict[str, str]`: _description_
+    `JsonResponse` (201): _If token and user_agent are correct, access_token and refresh_token will be returned_
     """
     access,refresh = await jwt_object.refresh(token.token, user_agent)
-    return {      #? make this Result?
+    return JSONResponse({
         "access_token": access,
         "refresh_token": refresh,
-    }
+    }, 201)
 
 
 @router.post('/logout',)
@@ -88,4 +88,4 @@ async def logout(jwt:JWTPayload=Depends(jwt_object)):
     `Result`: empty Result object
     """
     await jwt_object.logout(jwt.email, jwt.payload.get("jti"))
-    return Response()
+    return JSONResponse(Result(), 202)
